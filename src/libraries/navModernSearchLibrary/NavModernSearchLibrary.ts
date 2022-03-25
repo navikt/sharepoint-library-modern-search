a1ffc4ee7b94fa8a9987972b1a75c3b4ac77f6b0
Peter Holme Obrestad <peter.holme.obrestad@nav.no>
2022-03-25 11:10:52 +0100

    Add handlebar helper to fix visibility of planned messages

--- src/libraries/navModernSearchLibrary/NavModernSearchLibrary.ts
+++ src/libraries/navModernSearchLibrary/NavModernSearchLibrary.ts
@@ -9,6 +9,7 @@ import {
 } from "@pnp/modern-search-extensibility";
 import { Newslayout } from "./layouts/NewsLayout";
 import { ServiceMessageslayout } from "./layouts/ServiceMessagesLayout";
+import * as moment from 'moment';
 
 export class NavModernSearchLibrary implements IExtensibilityLibrary {
 
@@ -21,6 +22,26 @@ export class NavModernSearchLibrary implements IExtensibilityLibrary {
   }
 
   public registerHandlebarsCustomizations(namespace: typeof Handlebars) {
+    namespace.registerHelper('withGroupServiceMessages', (array: {}[], dateProp: string, priorityProp: string, options) => {
+      let result = '';
+      if (Array.isArray(array) && array.length > 0) {
+        // Sort all service messages by date reversed chronologically
+        array.sort((a,b) => new Date(b[dateProp]).getTime() - new Date(a[dateProp]).getTime());
+        // Sort service messages by priority
+        const priorityOrder = ['Kritisk', 'Advarsel', 'Informasjon'];
+        array.sort((a,b) => priorityOrder.indexOf(a[priorityProp]) - priorityOrder.indexOf(b[priorityProp]));
+        // Chunk by date to separate active and planned servicemessages
+        let subcontext = {'active': [],'planned': []};
+        array.map(value => {
+          if (moment(value[dateProp]).isSameOrBefore()) subcontext.active.push(value);
+          else subcontext.planned.push(value);
+        });
+        // Sort planned messages by date chronologically
+        subcontext.planned.sort((a,b) => new Date(a[dateProp]).getTime() - new Date(b[dateProp]).getTime());
+        result += options.fn(subcontext);
+      }
+      return result;
+    });
   }
 
   public getCustomLayouts(): ILayoutDefinition[] {
